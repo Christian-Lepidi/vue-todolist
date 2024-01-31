06bd3bd28fead29d141574dcb493a12b72d05558
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-31 15:54:01 +0100

    feat: add. list item with Vue

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,38 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      title: "Crea Task",
+      tasks: [
+        {
+          name: "Task 1",
+          done: true,
+        },
+        {
+          name: "Task 2",
+          done: true,
+        },
+        {
+          name: "Task 3",
+          done: false,
+        },
+        {
+          name: "Task 4",
+          done: true,
+        },
+        {
+          name: "Task 5",
+          done: false,
+        },
+        {
+          name: "Task 6",
+          done: false,
+        },
+        {
+          name: "Task 7",
+          done: true,
+        },
+      ],
+    };
   },
 }).mount("#app");
